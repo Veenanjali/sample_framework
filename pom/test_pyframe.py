a03ad63f310128e674634179bf0656e67db45e56
Veenanjali <91068676+Veenanjali@users.noreply.github.com>
2024-04-08 11:46:28 +0530

    Update test_pyframe.py

--- pom/test_pyframe.py
+++ pom/test_pyframe.py
@@ -13,7 +13,7 @@ def setup():
     chrome_options.add_argument('--headless')
 
     # Initialize WebDriver with ChromeOptions
-    driver = webdriver.Chrome(chrome_driver_path, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
     # Cleanup after test execution
     driver.quit()
